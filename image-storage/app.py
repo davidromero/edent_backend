6e2031a0519675065e54eb4b26c366556fd0793a
davidromero <d-romero@live.com>
2020-06-28 23:50:21 -0600

    image-storage service

--- image-storage/app.py
+++ image-storage/app.py
@@ -1,29 +1,28 @@
-from chalice import Chalice
+from chalice import Chalice, Response
+import boto3
 
 app = Chalice(app_name='image-storage')
 
+BUCKET = 'images.edent.backend'
+s3_client = boto3.client('s3')
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/upload/{file_name}', methods=['PUT'],
+    content_types=['application/octet-stream'])
+def upload_to_s3(file_name):
 
+    # get raw body of PUT request
+    body = app.current_request.raw_body
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    # write body to tmp file
+    tmp_file_name = '/tmp/' + file_name
+    with open(tmp_file_name, 'wb') as tmp_file:
+        tmp_file.write(body)
+
+    # upload tmp file to s3 bucket
+    s3_client.upload_file(tmp_file_name, BUCKET, file_name)
+
+    # TODO return img url
+
+    return Response(body='upload successful: {}'.format(file_name),
+                    status_code=200,
+                    headers={'Content-Type': 'text/plain'})
