aaa4c8b37e5b0ce64fcd1ec36e4dce986ed82587
davidromero <d-romero@live.com>
2020-07-05 19:34:51 -0600

    appointments service

--- appointments/app.py
+++ appointments/app.py
@@ -1,42 +1,54 @@
 import boto3 as boto3
-import logging
 from chalice import Chalice
 
-from chalicelib import database, custom_responses, calendar_api
-from chalicelib.config import TABLE_NAME, AWS_DEFAULT_REGION, cors_config
+from chalicelib import database, custom_responses
+from chalicelib.config import TABLE_NAME, AWS_DEFAULT_REGION
 
 app = Chalice(app_name='appointments')
-app.log.setLevel(logging.DEBUG)
 _DB = None
 
 
-@app.route('/', methods=['GET'], cors=cors_config)
+@app.route('/', methods=['GET'])
 def index():
     return custom_responses.get_base_res()
 
 
-@app.route('/appointments', methods=['GET'], cors=cors_config)
-def get_all_appointments():
-    params = app.current_request.query_params
-    if params is not None:
-        appointment_list = calendar_api.get_next_events(params.get('results'), 'primary')
-        return custom_responses.get_appointments_list(appointment_list)
-    appointment_list = get_app_db().get_next_events()
-    return custom_responses.get_appointments_list()
+@app.route('/contacts', methods=['GET'])
+def get_all_contacts():
+    active_contact_list = get_app_db().list_active_items()
+    return custom_responses.get_active_contacts(active_contact_list)
 
 
+@app.route('/contacts/{uid}', methods=['GET'])
+def get_contact(uid):
+    response = get_app_db().get_item(uid)
+    return custom_responses.get_response(response, uid)
 
-@app.route('/appointments/{uid}', methods=['POST'], cors=cors_config)
-def add_new_appointment(uid):
+
+@app.route('/contacts', methods=['POST'])
+def add_new_contact():
     body = app.current_request.json_body
-    new_item_id = get_app_db().add_item(appointment=body, patient_uid=uid)
+    new_item_id = get_app_db().add_item(contact=body)
     return custom_responses.post_response(new_item_id)
+
+
+@app.route('/contacts/{uid}', methods=['DELETE'])
+def delete_contact(uid):
+    response = get_app_db().inactivate_item(uid)
+    return custom_responses.edit_response(response, uid)
+
+
+@app.route('/contacts/{uid}', methods=['PUT'])
+def update_contact(uid):
+    body = app.current_request.json_body
+    response = get_app_db().update_item(uid, body)
+    return custom_responses.edit_response(response, uid)
 
 
 def get_app_db():
     global _DB
     if _DB is None:
-        _DB = database.DynamoDBAppointments(
+        _DB = database.DynamoDBContacts(
             boto3.Session().resource(service_name='dynamodb', region_name=AWS_DEFAULT_REGION).Table(TABLE_NAME)
         )
     return _DB
--- appointments/json_samples.md
+++ appointments/json_samples.md
@@ -4,34 +4,9 @@
 ```json
 {
     "start_time": "2020-06-26T09:00:00",
-    "duration": "30",
-    "first_name": "asdf",
-    "last_name": "gatica",
+    "patient_date": "asdf",
     "patient_uid": "1234",
-    "treatment_name": "idk",
-    "clinic_location": "chiquimula"
-}
-```
-
-## GET body
-
-```json
-{
-    "status": 200,
-    "payload": [
-        {
-            "id": "6sq36db374r3abb464rj2b9k6cs30b9ocdh6cb9jc8s32db564rj8p9gco",
-            "link": "https://www.google.com/calendar/event?eid=NnNxMzZkYjM3NHIzYWJiNDY0cmoyYjlrNmNzMzBiOW9jZGg2Y2I5amM4czMyZGI1NjRyajhwOWdjbyBhbGRvZ2F0aWNhMTIzQG0",
-            "title": "Something",
-            "start": {
-                "dateTime": "2020-06-04T08:00:00-06:00",
-                "timeZone": "America/Guatemala"
-            },
-            "end": {
-                "dateTime": "2020-06-04T09:00:00-06:00",
-                "timeZone": "America/Guatemala"
-            }
-        }
-    ]
+    "end_time": "2020-06-26T09:00:00",
+    "phone_number": "12345678"
 }
 ```
